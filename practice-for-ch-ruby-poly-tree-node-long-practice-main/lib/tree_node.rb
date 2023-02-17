17c526027e3dcdd6ccb5550da8810556b279d3da
Roderick Mendoza <rodmendoza07@gmail.com>
2023-02-17 14:08:37 -0800

    Complete parent specs

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,33 @@
 class PolyTreeNode
-  
+    def initialize(value, children = [])
+        @value = value
+        @parent = nil
+        @children = children
+    end
+
+    def parent=(node)
+        if node == nil
+            @parent = node
+            return
+        end
+        if node.children.include?(self)
+            return
+        end
+        if self.parent != nil
+            node.children << self
+            self.parent.children.delete(self)
+            @parent = node
+        else
+            node.children << self
+            @parent = node
+        end
+    end
+
+    def inspect
+        @children
+    end
+
+    attr_reader :children, :parent, :value
 end
+
+# node
