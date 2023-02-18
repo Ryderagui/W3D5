c19d7a766c1f53e609569e70afa6ea656bbf8876
Ryder_Aguilera <ryderaguilera@hotmail.com>
2023-02-17 16:38:03 -0800

    Added new_move_positions method

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/kpf.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/kpf.rb
@@ -3,10 +3,13 @@ require_relative 'tree_node.rb'
 class KnightPathFinder
     def initialize(start_pos)
         @start_pos = start_pos
-        @root_node = PolyTreeNode(start_pos)
+        @root_node = PolyTreeNode.new(start_pos)
         @tree = self.build_move_tree
         @considered_positions = Hash.new { |h, k| h[k] = false }
+        @considered_positions[start_pos] = true
     end
+
+    attr_reader :start_pos, :root_node, :considered_positions
 
     def self.valid_moves(pos)
         row, col = pos
@@ -16,14 +19,23 @@ class KnightPathFinder
         col_moves = [-2, -1, 1, 2]
         row_moves.each do |ele_1|
             col_moves.each do |ele_2|
-                moves << [ele_1, ele_2] if ele_1.abs  + ele_2.abs  == 3
+                moves << [ele_1+row, ele_2+col] if ele_1.abs  + ele_2.abs  == 3
             end
         end
         moves
     end
 
     def new_move_positions(pos)
-    end
+        moves = KnightPathFinder.valid_moves(pos)
+        new_moves = []
+        moves.each do |move|
+            if @considered_positions[move] == false
+                new_moves << move
+                @considered_positions[move] = true 
+            end                       
+        end
+        new_moves
+    end 
 
     def build_move_tree
     end
